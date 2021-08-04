7a8d56a04615e82238c09ff8360a488b93f136c4
fagojs <sushil.phago123@gmail.com>
2021-08-04 15:11:12 +0545

    imported dashboard-page here and used

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 
+import DashboardPage from "./components/dashboard/dashboard-page.component";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello there</h1>
+      <DashboardPage />
     </div>
   );
 }
