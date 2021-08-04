25d47e131723ce2ed1a34ecb4ecc1832655324a2
fagojs <sushil.phago123@gmail.com>
2021-08-04 21:43:24 +0545

    pagination functionality applied

--- src/components/dashboard/dashboard-preview.component.jsx
+++ src/components/dashboard/dashboard-preview.component.jsx
@@ -1,18 +1,36 @@
+import React, { useState } from "react";
+
 import DashboardItem from "./dashboard-item.component.jsx";
+import Pagination from "./../common/pagination";
 
 import "./dashboard-preview.css";
+
 const DashboardPreview = ({ data }) => {
   const { title, items } = data;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [notesPerPage] = useState(3);
+  const [totalNotes] = useState(items);
+
+  const lastIdx = currentPage * notesPerPage;
+  const firstIdx = lastIdx - notesPerPage;
+  const notesToDisplay = totalNotes.slice(firstIdx, lastIdx);
+
+  const handleClick = (pageNum) => setCurrentPage(pageNum);
+
   return (
     <div className="dashboard-preview">
       <span className="preview-title">{title}</span>
       <div className="preview-items">
-        {items
-          .filter((item, idx) => idx < 4)
-          .map((item) => (
-            <DashboardItem key={item.id} item={item} />
-          ))}
+        {notesToDisplay.map((note) => (
+          <DashboardItem key={note.id} item={note} />
+        ))}
       </div>
+      <Pagination
+        totalNotes={totalNotes.length}
+        notesPerPage={notesPerPage}
+        currentPage={currentPage}
+        handleClick={handleClick}
+      />
     </div>
   );
 };
