51e92ebb3516079cafbe99d1022127b5d37a0c93
fagojs <sushil.phago123@gmail.com>
2021-08-05 15:46:02 +0545

    form-input created required inside form and styling implemented

---
+++ src/components/common/form-input.jsx
@@ -0,0 +1,34 @@
+import "./form-input.css";
+
+const FormInput = ({ label, type, name, min, value, onChange }) => {
+  return (
+    <div>
+      {name === "description" ? (
+        <div className="form-input">
+          <label className="input-label">Description</label>
+          <textarea
+            name="description"
+            type="text"
+            value={value}
+            onChange={onChange}
+            required
+          />
+        </div>
+      ) : (
+        <div className="form-input">
+          <label className="input-label">{label}</label>
+          <input
+            type={type}
+            className="input-field"
+            name={name}
+            value={value}
+            onChange={onChange}
+            min={`${name === "page" || name === "price" ? `${min}` : "null"}`}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FormInput;
