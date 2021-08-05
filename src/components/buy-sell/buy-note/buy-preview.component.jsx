7aff84b7c71fea5c07abc15418ccdfd28c6152b7
fagojs <sushil.phago123@gmail.com>
2021-08-05 21:39:47 +0545

    pagination implemented

--- src/components/buy-sell/buy-note/buy-preview.component.jsx
+++ src/components/buy-sell/buy-note/buy-preview.component.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-import DashboardItem from "./../../dashboard/dashboard-item/dashboard-item.component";
+import BuyItem from "./buy-item.component";
+import Pagination from "./../../common/pagination/pagination";
+
+import "./buy-preview.css";
 
 const BuyPreview = ({ notes }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [notesPerPage] = useState(6);
+  const [totalNotes] = useState(notes);
+
+  const lastIdx = currentPage * notesPerPage;
+  const firstIdx = lastIdx - notesPerPage;
+  const totalNotesToShow = totalNotes.slice(firstIdx, lastIdx);
+
+  const handleClick = (pageNum) => {
+    return setCurrentPage(pageNum);
+  };
+
   return (
-    <div className="note-preview">
-      {notes.map((note) => (
-        <div className="note-items" key={note.id}>
-          <DashboardItem item={note} />
-        </div>
-      ))}
+    <div className="buy-preview">
+      <div className="buy-preview-items">
+        {totalNotesToShow.map((note) => (
+          <BuyItem item={note} key={note.id} />
+        ))}
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        notesPerPage={notesPerPage}
+        totalNotes={totalNotes.length}
+        handleClick={handleClick}
+      />
     </div>
   );
 };
