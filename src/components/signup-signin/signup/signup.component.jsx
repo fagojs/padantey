ff96e41eeeeff050726b719331fccd0d1f1d6fe7
fagojs <sushil.phago123@gmail.com>
2021-08-06 14:27:41 +0545

    styling of register form defined and implemented

--- src/components/signup-signin/signup/signup.component.jsx
+++ src/components/signup-signin/signup/signup.component.jsx
@@ -5,15 +5,18 @@ import FormInput from "../../common/form-input/form-input";
 import FacultySelect from "./form-select-field/faculty-select.component";
 import SemesterSelect from "./form-select-field/semester-select.component";
 import UniversitySelect from "./form-select-field/university-select.component";
+import CommonButton from "./../../common/commonbutton/common-button.component";
+
+import "./signup.css";
 
 const Signup = () => {
   return (
     <div className="signup-container">
-      <div className="sign-up-header">
+      <div className="signup-header">
         <h1>PADANTEY</h1>
-        <span align="center">Register with given information.</span>
+        <span>Register with given information.</span>
       </div>
-      <form className="signup-form-conatainer">
+      <form className="signup-form-container">
         <FormInput label="Username" type="text" name="username" />
         <FormInput label="Email" type="email" name="email" />
         <FormInput label="Password" type="password" name="password" />
@@ -22,6 +25,9 @@ const Signup = () => {
         <UniversitySelect label="University" name="university" />
         <FacultySelect label="Faculty" name="faculty" />
         <SemesterSelect label="Semester" name="semester" />
+        <div className="register-button">
+          <CommonButton>Register</CommonButton>
+        </div>
       </form>
       <p>
         Already have an account ? <a href="#">Log In</a>
