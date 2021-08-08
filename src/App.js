b287854d99ccf821607a60a9798841b306b7ca53
fagojs <sushil.phago123@gmail.com>
2021-08-08 11:31:26 +0545

    header component imported

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import SellNote from "../src/components/buy-sell/sell-note/sell-note.component";
 import BuyNote from "./components/buy-sell/buy-note/buy-note.component";
 import Signup from "./components/signup-signin/signup/signup.component";
 import Signin from "./components/signup-signin/signin/signin.component";
+import Header from "../src/components/header/header.component";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={DashboardPage} />
         <Route path="/buy-note" component={BuyNote} />
