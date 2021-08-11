c55c0b12d0f2000aa794190b312d8295d148e7fd
fagojs <sushil.phago123@gmail.com>
2021-08-11 14:19:57 +0545

    current-user extracted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
 import DashboardPage from "./components/dashboard/dashboard-page/dashboard-page.component";
 import SellNote from "../src/components/buy-sell/sell-note/sell-note.component";
@@ -8,9 +10,23 @@ import Signin from "./components/signup-signin/signin/signin.component";
 import Header from "../src/components/header/header.component";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState("");
+
+  useEffect(() => {
+    try {
+      const storedToken = localStorage.getItem("token");
+      const deocededToken = jwt_decode(storedToken);
+      setCurrentUser(deocededToken);
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.message);
+      }
+    }
+  }, []);
+
   return (
     <div className="App">
-      <Header />
+      <Header currentUser={currentUser} />
       <Switch>
         <Route exact path="/" component={DashboardPage} />
         <Route path="/buy-note" component={BuyNote} />
