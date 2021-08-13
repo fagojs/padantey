9b04630cd92776dd60e006c220122b55332f75e7
fagojs <sushil.phago123@gmail.com>
2021-08-13 10:37:26 +0545

    removed temp-data and passed currentUser

--- src/components/buy-sell/buy-note/buy-note.component.jsx
+++ src/components/buy-sell/buy-note/buy-note.component.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-import BUY_DATA from "../../../assets/buy-data";
 import BuyPreview from "./buy-preview.component";
 
-const BuyNote = () => {
-  const { items } = BUY_DATA[0];
+const BuyNote = ({ currentUser }) => {
   return (
     <div className="buy-note-container">
-      <BuyPreview notes={items} />
+      <BuyPreview currentUser={currentUser} />
     </div>
   );
 };
