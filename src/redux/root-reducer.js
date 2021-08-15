810b56461dd0ec1546e7da59b4e2fb1c190cde71
fagojs <sushil.phago123@gmail.com>
2021-08-15 11:07:24 +0545

    user-reducer combined

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,3 +1,7 @@
 import { combineReducers } from "redux";
 
-export default combineReducers({});
+import userReducer from "./reducers/user.reducers";
+
+export default combineReducers({
+  user: userReducer,
+});
