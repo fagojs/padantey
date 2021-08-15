31de9d2e5d0d5712833bced7dc69dd1a5674fa13
fagojs <sushil.phago123@gmail.com>
2021-08-15 18:54:48 +0545

    removed not-required datas and simplified styling

--- src/components/cart/cart-icon/cart-icon.component.jsx
+++ src/components/cart/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
-import { AiOutlineShopping } from "react-icons/ai";
+import { FaCartArrowDown } from "react-icons/fa";
 
 import "./cart-icon.css";
 
 const CartIcon = () => {
   return (
     <div className="cart-icon">
-      <AiOutlineShopping className="cart-box" />
-      <span className="quantity-label">0</span>
+      <FaCartArrowDown className="cart-box" />
     </div>
   );
 };
