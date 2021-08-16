0da24f62cee6175357ab84bb5bf4c7a6b521ef76
fagojs <sushil.phago123@gmail.com>
2021-08-16 16:56:19 +0545

    few adjustments made

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import jwt_decode from "jwt-decode";
 
+import Home from "../src/components/Home/home.component";
 import Header from "../src/components/header/header.component";
 import DashboardPage from "./components/dashboard/dashboard-page/dashboard-page.component";
 import SellNote from "../src/components/buy-sell/sell-note/sell-note.component";
@@ -10,8 +11,11 @@ import BuyNote from "./components/buy-sell/buy-note/buy-note.component";
 import Signup from "./components/signup-signin/signup/signup.component";
 import Signin from "./components/signup-signin/signin/signin.component";
 import Logout from "./components/logout/logout.component";
+import CartPage from "./components/cart/cart-page/cart-page.component";
 
 import setUser from "./redux/actions/user.actions";
+
+import "./App.css";
 
 function App({ setCurrentUser, currentUser }) {
   useEffect(() => {
@@ -30,33 +34,47 @@ function App({ setCurrentUser, currentUser }) {
     <div className="App">
       <Header />
       <Switch>
-        <Route exact path="/" component={DashboardPage} />
+        <Route exact path="/" component={Home} />
+        <Route
+          exact
+          path="/sold-note"
+          render={() => {
+            return currentUser ? <DashboardPage /> : <Signin />;
+          }}
+        />
         <Route
           path="/buy-note"
           exact
           render={() => {
-            return currentUser ? <BuyNote /> : <Redirect to="/signup" />;
+            return currentUser ? <BuyNote /> : <Signin />;
           }}
         />
         <Route
           path="/sell-note"
           exact
           render={() => {
-            return currentUser ? <SellNote /> : <Redirect to="/signup" />;
+            return currentUser ? <SellNote /> : <Signin />;
           }}
         />
         <Route
           path="/signup"
           exact
           render={() => {
-            return currentUser ? <Redirect to="/" /> : <Signup />;
+            return currentUser ? <DashboardPage /> : <Signup />;
           }}
         />
         <Route
           path="/signin"
           exact
           render={() => {
-            return currentUser ? <Redirect to="/" /> : <Signin />;
+            return currentUser ? <DashboardPage /> : <Signin />;
+          }}
+        />
+        <Route
+          path="/cart-page"
+          exact
+          render={() => {
+            return currentUser ? <CartPage /> : <Signin />;
           }}
         />
         <Route path="/logout" component={Logout} />
