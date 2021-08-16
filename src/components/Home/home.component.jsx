781de852d968726eca16b2b5e6e8fa010af58253
fagojs <sushil.phago123@gmail.com>
2021-08-16 16:52:42 +0545

    home-component defined

---
+++ src/components/Home/home.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <p>
+        A warm welcome to you. We present you this platform as to buy and sell
+        your note books. We hope you will have good interactions ahead. Thank
+        You.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
